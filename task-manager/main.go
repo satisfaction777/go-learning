36a4d05803b239506218c6392915040df3b8079c
satisfaction777 <pavelnaymov0@gmail.com>
2026-05-11 14:34:26 +0700

    add logger middleware

--- task-manager/main.go
+++ task-manager/main.go
@@ -4,14 +4,25 @@ import (
 "task-manager/db"        
 "task-manager/handlers"
 "github.com/gin-gonic/gin"
+"fmt"
 )
 
+
+func Logger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Printf("→ %s %s\n", c.Request.Method, c.Request.URL.Path)
+		c.Next()
+		fmt.Printf("← %d\n", c.Writer.Status())
+	}
+}
 
 
 func main() {
 db.ConnectDB()
 
 r := gin.Default()
+
+r.Use(Logger())
 
 r.GET("/tasks/:id", handlers.GetTask)
 r.GET("/tasks", handlers.GetTasks)
